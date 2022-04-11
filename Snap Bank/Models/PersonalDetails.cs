8eee41d3a352b6e533b1e4180f25866bc2f9aae5
SaiSindhuRudraraju <100202255+SaiSindhuRudraraju@users.noreply.github.com>
2022-04-11 18:02:07 +0530

    initial commit

---
+++ Snap Bank/Models/PersonalDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Snap_Bank.Models
+{
+    [Table("PersonalDetails")]
+    public class PersonalDetails
+    {
+        [Key]
+        public int UserId { get; set; }
+        public int AccountNumber { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public String Gender { get; set; }
+        public String Gmail { get; set; }
+        public String MobileNumber { get; set; }
+    }
+}
---
+++ Snap Bank/Models/SecurityQuestions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Snap_Bank.Models
+{
+    [Table("SecurityQuestions")]
+    public class SecurityQuestions
+    {
+        [Key]
+        public int UserId { get; set; }
+        public int AccountNumber { get; set; }
+        public String BirthPlace { get; set; }
+        public String PetName { get; set; }
+        public String FavouriteFood { get; set; }
+    }
+}
